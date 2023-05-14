b65b413571b2b4c89bbfa8cebf27a1d477050878
John Bob Ayensu <johnbayensu@gmail.com>
2023-05-14 08:18:31 +0000

    updated connection string

--- Classes/Login.php
+++ Classes/Login.php
@@ -55,7 +55,7 @@ class Login
         } elseif (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
 
             // create a database connection, using the constants from config/db.php (which we loaded in index.php)
-            $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            $this->db_connection = new mysqli("localhost", "root", "742166&Jab", "e-portfolio");
 
             // change character set to utf8 and check it
             if (!$this->db_connection->set_charset("utf8")) {
